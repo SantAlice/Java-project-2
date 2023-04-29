b90e02d0d3dc86ba6567b519445cfdf9a46e9baf
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-04-30 00:53:29 +0600

    Modified gitignor, created java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 24890;
+        int oneBonusMile = 20;
+        int bonus = (ticketPrice / oneBonusMile);
+        System.out.println(bonus);
+    }
+}
